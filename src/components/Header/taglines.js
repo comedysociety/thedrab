8165014bef7c367c53e73025e7d80c7b5dedcf50
James Canning <james@brudil.com>
2017-07-14 08:10:54 +0100

    Fix tagline typo

--- src/components/Header/taglines.js
+++ src/components/Header/taglines.js
@@ -3,7 +3,7 @@ export default [
   'Beyond tustworthy.',
   'Content for contents sake.',
   'Be content, not content',
-  'The exit shute of The Butterfly Effect',
+  'The exit chute of The Butterfly Effect',
   'The content equivalent of carbs',
   'Your life. Our ad views.'
 ];
