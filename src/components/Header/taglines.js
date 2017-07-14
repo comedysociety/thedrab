65646ece49442df58bad3f12238d147af426e32f
Reece Selby <reece.selby@gmail.com>
2017-07-14 19:21:37 +0100

    "fiXEd tAGliNe tYpO"

--- src/components/Header/taglines.js
+++ src/components/Header/taglines.js
@@ -1,9 +1,11 @@
 export default [
   'News you don\'t need to trust.',
-  'Beyond tustworthy.',
+  'Beyond trustworthy.',
   'Content for contents sake.',
   'Be content, not content',
   'The exit chute of The Butterfly Effect',
   'The content equivalent of carbs',
-  'Your life. Our ad views.'
+  'Your life. Our ad views.',
+  'Too big for our news boots',
+  'New media means new standards of journalism'
 ];
